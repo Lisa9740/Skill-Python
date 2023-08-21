14927e5d9dc165550ecb264a03d304147f95e438
Lisa9740 <barretalison@gmail.com>
2023-08-21 17:59:12 +0400

    Create hangman-challenge.py

---
+++ hangman-challenge.py
@@ -0,0 +1,8 @@
+#create a greeting
+# create a word list
+# randomly choose a word from the lit you have created
+# ask a user to guess a letter
+#bonus make the program take the input from the user and make it lowercase
+# check if the letter is in the word
+
+
